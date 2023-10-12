f6081827675aa89ca0915fc0dea93e73291cdf40
murtazaaali <alim02818@gmail.com>
2023-10-12 12:07:14 +0500

    commit new changes

--- MainServer.js
+++ MainServer.js
@@ -55,11 +55,12 @@ const Server = express();
 //   })
 // );
 
-// const allowedOrigins = "http://localhost:3000";
-const allowedOrigins = [
-  "https://bvl-academy-1ik1jzgnn-ali-murtazas-projects.vercel.app",
-  "http://localhost:3000",
-];
+const allowedOrigins =
+  "https://bvl-academy-9tq1rkxeg-ali-murtazas-projects.vercel.app";
+// const allowedOrigins = [
+//   "https://bvl-academy-1ik1jzgnn-ali-murtazas-projects.vercel.app",
+//   "http://localhost:3000",
+// ];
 Server.use(
   cors({
     origin: (origin, callback) => {
